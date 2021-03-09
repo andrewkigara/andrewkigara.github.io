8142725b35bf82cb1c1c14c642a6e19bc39f09a3
Andrew Kigara <andrewkigara@gmail.com>
2021-03-08 23:37:34 -0500

    reddit dude

--- new-website/src/App.js
+++ new-website/src/App.js
@@ -1,9 +1,10 @@
 import './App.css';
+import redditDude from './assets/redditDude.png';
 
 function App() {
   return (
-    <div className="container">
-      <h1>Hey</h1>
+    <div className="container text-center justify-content-center">
+      <img className="redditDude mt-3" src={redditDude} alt="reddit dude"/>
     </div>
   );
 }
