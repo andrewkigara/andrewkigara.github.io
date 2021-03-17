8fbfeedd8cffe3eaeb53c1876cd3e3784d93fb39
Andrew Kigara <andrewkigara@gmail.com>
2021-03-17 08:50:57 -0400

    new layout?

--- new-website/src/Pages/Home/Home.js
+++ new-website/src/Pages/Home/Home.js
@@ -1,31 +1,44 @@
 import React from 'react';
 import Typical from 'react-typical';
+import redditDude from '../../Assets/redditDude.png';
 
 export const Home = () => {
     return (
         <div className="container">
-            <h1>Hello</h1>
-            <h2>My name is <em>Andrew</em></h2>
-            <p>I'm a {' '}
-                <Typical
-                    loop={Infinity}
-                    wrapper="b"
-                    steps={[
-                        'developer',
-                        1000,
-                        'designer',
-                        1000,
-                        'photographer',
-                        1000,
-                        'programmer',
-                        1000,
-                        'analyst',
-                        1000,
-                    ]}
-                />
+            <div className="row">
+                {/* Reddit dude */}
+                <div className="col align-self-start">
+                    <img src={redditDude} alt="reddit man" height="auto" width="60%"/>
+                </div>
+                {/* Auto-type */}
+                <div className="col align-self-center">
+                    <h1>Hello :)</h1>
+                    <h3>My name is André</h3>
+                    <p>I am {' '}
+                        <Typical
+                            loop={Infinity}
+                            wrapper="b"
+                            steps={[
+                                'a software developer',
+                                1000,
+                                'a web designer',
+                                1000,
+                                'a web developer',
+                                1000,
+                                'a graphic designer',
+                                1000,
+                                'an amazing Twin according to my Twin ',
+                                1000,
+                            ]}
+                        />
+                    </p>
+                </div>
+                {/* Buttons */}
 
-            </p>
+            </div>
 
         </div>
     )
 }
+
+
