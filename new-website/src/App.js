a89518179adaab141c594f68c53babc910c55e7c
Andrew Kigara <andrewkigara@gmail.com>
2021-03-16 23:10:26 -0400

    new developments

--- new-website/src/App.js
+++ new-website/src/App.js
@@ -1,13 +1,8 @@
 import React from 'react';
-// import AutoType from './pages/AutoType';
-import './App.css';
-import redditDude from './assets/redditDude.png';
 
 function App() {
   return (
-    <div className="container text-center justify-content-center">
-      <img className="redditDude mt-3" src={redditDude} alt="reddit dude"/>
-      {/* <AutoType /> */}
+    <div className="container">
     </div>
   );
 }
