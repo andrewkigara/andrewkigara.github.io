6b267b2993a7dba95403fa35b63dad33a1f580e0
Andrew Kigara <andrewkigara@gmail.com>
2022-06-30 00:32:59 -0400

    summary

---
+++ new-website/src/Pages/Recommendations/Recommendations.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+export const Recommendations = () => {
+    return (
+        <div className="container">
+            <iframe src="https://www.linkedin.com/embed/feed/update/urn:li:share:6781305736254251008" height="804" width="504" frameborder="0" allowfullscreen="" title="Embedded post"></iframe>
+
+        </div>
+    )
+}
+
+
